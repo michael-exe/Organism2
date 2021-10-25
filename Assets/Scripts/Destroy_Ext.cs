2cb57f1505299c2081ffa4a81d068ae3bc5db4a9
Michelangelo <michelangelo.bedetti@gmail.com>
2021-10-26 01:40:49 +0200

    Streamline for fiverr

--- Assets/Scripts/Destroy_Ext.cs
+++ Assets/Scripts/Destroy_Ext.cs
@@ -6,15 +6,13 @@ public class Destroy_Ext : MonoBehaviour
 {
     void Update()
     {
-        Debug.Log("exhist");
+
     }
     private void OnTriggerEnter(Collider other)
     {
-        Debug.Log("collision");
         if (other.gameObject.tag == "Int_Molecule")
         {
             Destroy(this.gameObject);
-            Debug.Log("collision if");
         }
     }
 } //https://youtu.be/LNLVOjbrQj4
