4ec07af63422c789f71e9c7ec6671958bf37c1c8
Michelangelo <michelangelo.bedetti@gmail.com>
2021-11-03 22:41:04 +0100

    Bug Fix: Change tag instead of instance

--- Assets/Scripts/AttachmentController.cs
+++ Assets/Scripts/AttachmentController.cs
@@ -15,12 +15,6 @@ public class AttachmentController : MonoBehaviour
     public Transform UL_Holder;
     public Transform DR_Holder;
     public Transform DL_Holder;
-    public Transform Int_MoleculePrefab;
-    private bool returnal;
-    bool continue_checking;
-    int spawnCount;
-
-    //bool hit;
 
     private void Start()
     {
@@ -36,133 +30,60 @@ public class AttachmentController : MonoBehaviour
         UL_Attach();
         DR_Attach();
         DL_Attach();
-        continue_checking = true;
 
-        //if (continue_checking)
-        //{
-        //    continue_checking &= !UR_Attach();
-        //    continue_checking &= !UL_Attach();
-        //    continue_checking &= !DR_Attach();
-        //    continue_checking &= !DL_Attach();
-        //}
     }
 
     //DETECTOR(ext) referencing HOLDER(int)
-    bool UR_Attach()
+    void UR_Attach()
     {
         
 
         RaycastHit2D Ext2Int = Physics2D.Raycast(UR_Detector.position, Vector2.zero, -0);
+        //Not really Ext2Int anymore
         
 
-        if (Ext2Int.collider != null && Ext2Int.collider.tag == "Ext_Molecule" && continue_checking == true)
+        if (Ext2Int.collider != null && Ext2Int.collider.tag == "Ext_Molecule")
         {
-            Ext2Int.collider.gameObject.tag = "Untagged";
-            Destroy(Ext2Int.collider.gameObject);
-         
-            Transform Int_Molecule_Inst = Instantiate(this.Int_MoleculePrefab);
-            Int_Molecule_Inst.transform.parent = UR_Holder;
-            Int_Molecule_Inst.transform.position = UR_Holder.position;
-
-            Debug.Log("UR_Attach");
-
-            return false;
+            //Destroy(Ext2Int.collider.gameObject);
+            Ext2Int.collider.transform.parent = UR_Holder;
+            Ext2Int.collider.transform.position = UR_Holder.position;
+            Ext2Int.collider.tag = "Int_Molecule";
         }
 
-        else
-        {
-            return true;
-        }
-        
-        //if (Input.GetKey(KeyCode.X))
-        //{
-        //    Ext2Int.collider.gameObject.transform.parent = null;
-        //}
     }
-    bool UL_Attach()
+    void UL_Attach()
     {
         RaycastHit2D Ext2Int = Physics2D.Raycast(UL_Detector.position,Vector2.zero, -0);
 
-        if (Ext2Int.collider != null && Ext2Int.collider.tag == "Ext_Molecule" && continue_checking == true)
-        {   
-            Ext2Int.collider.gameObject.tag = "Untagged";
-            Destroy(Ext2Int.collider.gameObject);
-            
-            Transform Int_Molecule_Inst = Instantiate(this.Int_MoleculePrefab);
-            Int_Molecule_Inst.transform.parent = UL_Holder;
-            Int_Molecule_Inst.transform.position = UL_Holder.position;
-
-            Debug.Log("UL_Attach");
-
-            return false;
-        }
-
-        else
+        if (Ext2Int.collider != null && Ext2Int.collider.tag == "Ext_Molecule")
         {
-            return true;
+            Ext2Int.collider.transform.parent = UL_Holder;
+            Ext2Int.collider.gameObject.transform.position = UL_Holder.position;
+            Ext2Int.collider.gameObject.tag = "Int_Molecule";
         }
-
-        //if (Input.GetKey(KeyCode.X))
-        //{
-        //    Ext2Int.collider.gameObject.transform.parent = null;
-        //}
     }
-    bool DR_Attach()
+    void DR_Attach()
     {
         RaycastHit2D Ext2Int = Physics2D.Raycast(DR_Detector.position, Vector2.zero, 0);
 
-        if (Ext2Int.collider != null && Ext2Int.collider.tag == "Ext_Molecule" && continue_checking == true)
-        { 
-            Ext2Int.collider.gameObject.tag = "Untagged";
-            Destroy(Ext2Int.collider.gameObject);
-           
-            Transform Int_Molecule_Inst = Instantiate(this.Int_MoleculePrefab);
-            Int_Molecule_Inst.transform.parent = DR_Holder;
-            Int_Molecule_Inst.transform.position = DR_Holder.position;
-
-            Debug.Log("DR_Attach");
-
-            return false;
-        }
-
-        else
+        if (Ext2Int.collider != null && Ext2Int.collider.tag == "Ext_Molecule")
         {
-            return true;
+            Ext2Int.collider.transform.parent = DR_Holder;
+            Ext2Int.collider.gameObject.transform.position = DR_Holder.position;
+            Ext2Int.collider.gameObject.tag = "Int_Molecule";
         }
-
-        //if (Input.GetKey(KeyCode.X))
-        //{
-        //    Ext2Int.collider.gameObject.transform.parent = null;
-        //}
     }
-    bool DL_Attach()
+    void DL_Attach()
     {
        
         RaycastHit2D Ext2Int = Physics2D.Raycast(DL_Detector.position, Vector2.zero, 0);
 
-        if (Ext2Int.collider != null && Ext2Int.collider.tag == "Ext_Molecule" && continue_checking == true)
-        {             
-            Ext2Int.collider.gameObject.tag = "Untagged";
-            Destroy(Ext2Int.collider.gameObject);
-           
-            Transform Int_Molecule_Inst = Instantiate(this.Int_MoleculePrefab);
-            Int_Molecule_Inst.transform.parent = DL_Holder;
-            Int_Molecule_Inst.transform.position = DL_Holder.position;
-
-            Debug.Log("DL_Attach");
-
-            return false;
-        }
-
-        else
+        if (Ext2Int.collider != null && Ext2Int.collider.tag == "Ext_Molecule")
         {
-            return true;
+            Ext2Int.collider.transform.parent = DL_Holder;
+            Ext2Int.collider.gameObject.transform.position = DL_Holder.position;
+            Ext2Int.collider.gameObject.tag = "Int_Molecule";
         }
-
-        //if (Input.GetKey(KeyCode.X))
-        //{
-        //    Ext2Int.collider.gameObject.transform.parent = null;
-        //}
 
     }
 
